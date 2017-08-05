cf42b308f094057135f8f2980b05243a49a83da6
Mary Elizabeth <mary.elizabeth.aleman@gmail.com>
2017-08-04 19:27:50 -0500

    Assignment 9 completed.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,30 @@
 var pointsArray = document.getElementsByClassName('point');
 
-     var animatePoints = function(points) {
-     var revealPoint = function(index) {
-       points[index].style.opacity = .7;
-       points[index].style.transform = "scaleX(1) translateY(0)";
-       points[index].style.msTransform = "scaleX(1) translateY(0)";
-       points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-       points[index].style.transition= "all 1s ease-in-out";
-    }
-       for (var i = 0; i < points.length; i++){
-         revealPoint(i);
-       }
-     }
+var revealPoint = function(point) {
+    point.style.opacity = .7;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    point.style.transition= "all 1s ease-in-out";
+};
+
+  var animatePoints = function(points) {
+    forEach(points, revealPoint);
+  };
+
+
+      // var revealPoint = function(index) {
+        // points[index].style.opacity = .7;
+         //points[index].style.transform = "scaleX(1) translateY(0)";
+         //points[index].style.msTransform = "scaleX(1) translateY(0)";
+         //points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+         //points[index].style.transition= "all 1s ease-in-out";
+      //}
+    //for (var i = 0; i < points.length; i++){
+    //revealPoint(i);
+    //}
+
+    //}
 
 window.onload = function() {
   if (window.innerHeight > 950) {
