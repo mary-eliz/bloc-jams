554c02391b23342ec8407dc43beecc1439ccd274
Mary Elizabeth <mary.elizabeth.aleman@gmail.com>
2017-08-13 23:20:42 -0500

    Checkpoint 12 Play-Pause: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -50,7 +50,7 @@ var albumCamada = {
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -84,9 +84,34 @@ var createSongRow = function(songNumber, songName, songLength) {
      }
  };
 
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
-     setCurrentAlbum(albumMarconi);
+     setCurrentAlbum(albumPicasso);
+
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         console.log(event.target);
+          // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+                    event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+               }
+     });
+
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
  };
+
+
+
 
  var threeAlbums = [albumPicasso, albumMarconi, albumCamada]
  var i=0;
